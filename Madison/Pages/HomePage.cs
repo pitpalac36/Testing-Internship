0b4e1944c558dffb94a16e8f52a7ea8ae01b8044
Maraciuc Claudia <claudia.maraciuc@evozon.com>
2021-07-30 15:46:33 +0300

    test login functionality

--- Madison/Pages/HomePage.cs
+++ Madison/Pages/HomePage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Madison.Helpers;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,27 @@ namespace Madison.Pages
 {
     public class HomePage
     {
+
+        #region Selectors
+
+         private readonly By accountElement = By.CssSelector(".account-cart-wrapper > a");
+         private readonly By menuElements = By.CssSelector("#header-account>.links>ul li");
+        #endregion
+
+        public void ClickOnAccount()
+        {
+            Driver.webDriver.FindElement(accountElement).Click();
+        }
+
+        private void SelectMyAccountMenu(string accountMenu)
+        {
+            Driver.webDriver.FindElements(menuElements).First(item => item.Text == accountMenu).Click();
+        }
+
+        public void ClickLogInButton()
+        {
+            SelectMyAccountMenu("Log In");
+           
+        }
     }
 }
