2febf2621624f73d45672a76f7d03bdb2024d5c9
horgaemilia <emiliahorga15@gmail.com>
2021-07-30 14:02:30 +0300

    fixed TestCart class

--- Madison/Tests/TestCart.cs
+++ Madison/Tests/TestCart.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace Madison.Tests
 {
     [TestClass]
-    class TestCart : BaseTest
+    public class TestCart : BaseTest
     {
         public void GoToCart()
         {
