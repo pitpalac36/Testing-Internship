6491e1f52eda6617d615fa66f8f5822a9f701642
Laura Dunca <laura.dunca@evozon.com>
2021-07-30 14:26:24 +0300

    wishlist button redirects test + product quantity updates correctly test (hardcoded login)

--- Madison/Pages/MyWishlistPage.cs
+++ Madison/Pages/MyWishlistPage.cs
@@ -6,8 +6,11 @@ namespace Madison.Pages
     public class MyWishlistPage
     {
         #region Selectors
-        private static readonly By _accountSelector = By.CssSelector(".account-cart-wrapper span+span");
-        private static readonly By _myWishlistSelector = By.CssSelector("a[title^='My Wishlist']");
+        private readonly By _accountSelector = By.CssSelector(".account-cart-wrapper span+span");
+        private readonly By _myWishlistSelector = By.CssSelector("a[title^='My Wishlist']");
+        private readonly By _myWishlistHeaderSelector = By.CssSelector(".my-wishlist h1");
+        private readonly By _firstItemQuantityCell = By.CssSelector(".first.odd input");
+        private readonly By _updateWishlistFirstButton = By.CssSelector(".first.odd button");
         #endregion
 
         public void ClickOnAccount()
@@ -24,6 +27,31 @@ namespace Madison.Pages
         {
             Driver.webDriver.FindElement(_myWishlistSelector).Click();
             WaitHelpers.WaitUntilDOcumentReady();
+        }
+
+        public bool IsRedirectedToWishlist()
+        {
+            return Driver.webDriver.FindElement(_myWishlistHeaderSelector).Displayed;
+        }
+
+        public int ChangeQuantity()
+        {
+            var quantityInput = Driver.webDriver.FindElement(_firstItemQuantityCell);
+            var newQuantity = int.Parse(quantityInput.GetAttribute("value")) * 2;
+            quantityInput.Clear();
+            quantityInput.SendKeys(newQuantity.ToString());
+            return newQuantity;
+        }
+
+        public void ClickOnUpdateItem()
+        {
+            Driver.webDriver.FindElement(_updateWishlistFirstButton).Click();
+            WaitHelpers.WaitUntilDOcumentReady();
+        }
+
+        public int ItemQuantity()
+        {
+            return int.Parse(Driver.webDriver.FindElement(_firstItemQuantityCell).GetAttribute("value"));
         }
     }
 }
--- Madison/Tests/WishlistTest.cs
+++ Madison/Tests/WishlistTest.cs
@@ -1,12 +1,15 @@
 ﻿using FluentAssertions;
 using Madison.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 
 namespace Madison.Tests
 {
     [TestClass]
     public class WishlistTest: BaseTest
     {
+
+
         [TestMethod]
         public void MyWishlistButtonIsDisplayed()
         {
@@ -14,6 +17,52 @@ namespace Madison.Tests
             Pages.MyWishlistPage.IsWishlistButtonDisplayed().Should().BeTrue();
         }
 
+        [TestMethod]
+        public void ClickOnMyWishlistButtonRedirects()
+        {
+            Login();
+            Pages.MyWishlistPage.ClickOnAccount();
+            Pages.MyWishlistPage.ClickOMyWishlist();
+            Pages.MyWishlistPage.IsRedirectedToWishlist().Should().BeTrue();
+        }
 
+        [TestMethod]
+        public void WishlistItemQuantityUpdatesCorrectly()
+        {
+            Login();
+            Pages.MyWishlistPage.ClickOnAccount();
+            Pages.MyWishlistPage.ClickOMyWishlist();
+            var newQuantity = Pages.MyWishlistPage.ChangeQuantity();
+            Pages.MyWishlistPage.ClickOnUpdateItem();
+            Pages.MyWishlistPage.ItemQuantity().Should().Be(newQuantity);
+        }
+
+
+
+
+        public void Login()
+        {
+
+            // go to Maddison
+            Driver.webDriver.Navigate().GoToUrl("http://qa2.dev.evozon.com/");
+
+            // click on account
+            Pages.MyWishlistPage.ClickOnAccount();
+
+            // go to login
+            var login = Driver.webDriver.FindElement(By.CssSelector(".links li.last a[title='Log In']"));
+            login.Click();
+
+            var form = Driver.webDriver.FindElement(By.CssSelector("form#login-form"));
+            form.Displayed.Should().BeTrue();
+
+            var email = Driver.webDriver.FindElement(By.Name("login[username]"));
+            email.SendKeys("ana.ana@outlook.com");
+            var password = Driver.webDriver.FindElement(By.Name("login[password]"));
+            password.SendKeys("1234567");
+
+            var loginBtn = Driver.webDriver.FindElement(By.Id("send2"));
+            loginBtn.Submit();
+        }
     }
 }
