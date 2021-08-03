7671cae8cb54a48d7ec7a6adc0320affa808030b
Laura Dunca <laura.dunca@evozon.com>
2021-08-03 10:33:01 +0300

    issue #57 resolved

--- Madison/Pages/MyWishlistPage.cs
+++ Madison/Pages/MyWishlistPage.cs
@@ -14,7 +14,7 @@ namespace Madison.Pages
         private readonly By _firstItemQuantityCell = By.CssSelector("#wishlist-view-form .first.odd input");
         private readonly By _updateWishlistFirstButton = By.CssSelector("#wishlist-table button");
         private readonly By _updateWishlistBigButton = By.CssSelector(".buttons-set.buttons-set2 button:last-child");
-        private readonly By _shareWishlistButton = By.CssSelector(".buttons-set.buttons-set2 button");
+        private readonly By _shareWishlistButton = By.CssSelector(".buttons-set [name='save_and_share']");
         private readonly By _shareWishlistForm = By.CssSelector(".col-main");
         private readonly By _shareWishlistEmailTextArea = By.CssSelector("textarea[Name='emails']");
         private readonly By _shareWishlistFinalButton = By.CssSelector(".form-buttons button");
