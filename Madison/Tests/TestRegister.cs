05d8ee803faa909f349c18b1fa753e7266aaefcd
Maraciuc Claudia <claudia.maraciuc@evozon.com>
2021-08-03 15:21:38 +0300

    account and test register classes

---
+++ Madison/Tests/TestRegister.cs
@@ -0,0 +1,18 @@
+﻿using Madison.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madison.Tests
+{
+    [TestClass]
+    class TestRegister:BaseTest 
+    {
+        [DataTestMethod]
+        
+    }
+}
