6eeca90387266c35c7847260769c75fca5678527
Laura Dunca <laura.dunca@evozon.com>
2021-08-06 09:23:58 +0300

    deleted unused stuff

--- Madison/Pages/CheckoutPage.cs
+++ Madison/Pages/CheckoutPage.cs
@@ -2,10 +2,6 @@
 using NsTestFrameworkUI.Pages;
 using OpenQA.Selenium;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Madison.Helpers;
 using OpenQA.Selenium.Support.UI;
 
@@ -31,21 +27,18 @@ namespace Madison.Pages
         private readonly By _billingCityInput = By.CssSelector("#billing\\:city");
         private readonly By _billingStateDropdownSelect = By.CssSelector("#billing\\:region_id");
         private readonly By _billingZipCodeInput = By.CssSelector("#billing\\:postcode");
-        private readonly By _billingCountryDropdownSelect = By.CssSelector("#billing\\:country_id");
         private readonly By _billingTelephoneInput = By.CssSelector("#billing\\:telephone");
         private readonly By _continueButtonShippingInformation = By.CssSelector("#shipping-buttons-container > button");
         private readonly By _continueButtonFromShippingMethod = By.CssSelector("#shipping-method-buttons-container > button");
         private readonly By _editCheckoutMethod = By.CssSelector("#opc-login > div.step-title > a");
         private readonly By _registerAndCheckoutRadioButton = By.CssSelector("#login\\:register");
         private readonly By _editShippingInformation = By.CssSelector("#opc-shipping > div.step-title > a");
-        private readonly By _proceedCheckoutButton = By.CssSelector(".button.btn-proceed-checkout.btn-checkout  span span ");
         private readonly By _shippingFirstNameInput = By.CssSelector("#shipping\\:firstname");
         private readonly By _shippingLastNameInput = By.CssSelector("#shipping\\:lastname");
         private readonly By _shippingAddressInput = By.CssSelector("#shipping\\:street1");
         private readonly By _shippingCityInput = By.CssSelector("#shipping\\:city");
         private readonly By _shippingStateDropdownSelect = By.CssSelector("#shipping\\:region_id");
         private readonly By _shippingZipCodeInput = By.CssSelector("#shipping\\:postcode");
-        private readonly By _shippingCountryDropdownSelect = By.CssSelector("#shipping\\:country_id");
         private readonly By _shippingTelephoneInput = By.CssSelector("#shipping\\:telephone");
         private readonly By _checkoutSuccessMessage = By.CssSelector(".sub-title");
         #endregion
@@ -56,29 +49,9 @@ namespace Madison.Pages
             WaitHelpers.WaitForDocumentReadyState();
         }
 
-        public void ClickRegisterAndCheckout()
-        {
-            _registerAndCheckoutRadioButton.ActionClick();
-        }
-
-        public void ClickEditShippingInformation()
-        {
-            _editShippingInformation.ActionClick();
-        }
-
-        public void ClickEditCheckoutMethod()
-        {
-            _editCheckoutMethod.ActionClick();
-        }
-
         public void ClickContinueFromShippingMethod()
         {
             _continueButtonFromShippingMethod.ActionClick();
-        }
-
-        public void ClickContinueFromShippingInformation()
-        {
-            _continueButtonShippingInformation.ActionClick();
         }
 
         public void ClickContinueCheckout()
@@ -99,31 +72,10 @@ namespace Madison.Pages
             WaitHelpers.WaitForDocumentReadyState();
         }
 
-        public void ChooseShipToThisAddress()
-        {
-            _shipToThisAddressRadioButton.ActionClick();
-        }
-
-        public void ChooseShipToDifferentAddress()
-        {
-            _shipToDifferentAddressRadionButton.ActionClick();
-        }
-
         public void ApplyFreeShipping()
         {
             if(_freeShippingRadioButton.IsElementPresent())
                 _freeShippingRadioButton.ActionClick();
-        }
-
-        public void ApplyFlatRateShipping()
-        {
-            if (_flatRateShippingRadionButton.IsElementPresent())
-                _flatRateShippingRadionButton.ActionClick();
-        }
-
-        public void AddGift()
-        {
-            _addGiftCheckbox.ActionClick();
         }
 
 
@@ -152,25 +104,6 @@ namespace Madison.Pages
         {
             new WebDriverWait(Browser.WebDriver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementIsVisible(_checkoutSuccessMessage));
             return _checkoutSuccessMessage.IsElementPresent();    
-        }
-
-        public void FillShippingForm()
-        {
-            _shippingFirstNameInput.ClearField();
-            _shippingLastNameInput.ClearField();
-            _shippingAddressInput.ClearField();
-            _shippingCityInput.ClearField();
-            _shippingZipCodeInput.ClearField();
-            _shippingTelephoneInput.ClearField();
-
-            _shippingFirstNameInput.ActionSendKeys(BillingInformation.FirstName);
-            _shippingLastNameInput.ActionSendKeys(BillingInformation.LastName);
-            _shippingAddressInput.ActionSendKeys(BillingInformation.Address);
-            _shippingCityInput.ActionSendKeys(BillingInformation.City);
-            _shippingZipCodeInput.ActionSendKeys(BillingInformation.ZIP);
-            _shippingTelephoneInput.ActionSendKeys(BillingInformation.Telephone);
-            SelectElement cityDropdown = new SelectElement(Browser.WebDriver.FindElement(_shippingStateDropdownSelect));
-            cityDropdown.DeselectByIndex(2);
         }
 
 
